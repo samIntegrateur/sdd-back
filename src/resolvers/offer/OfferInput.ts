f0187e3c3ff1fee5b120bc8d271a1d16041ad29e
Samuel Desbos <samuel.desbos@gmail.com>
2020-09-10 21:10:05 +0200

    Add image upload logic to the offerResolver

--- src/resolvers/offer/OfferInput.ts
+++ src/resolvers/offer/OfferInput.ts
@@ -1,19 +1,23 @@
 import { Field, InputType } from 'type-graphql';
 import { Offer } from '../../entities/Offer';
-import { IsUrl, Length } from 'class-validator';
+import { Length } from 'class-validator';
+import { FileUpload, GraphQLUpload } from 'graphql-upload';
 
 @InputType()
 export class OfferInput implements Partial<Offer> {
 
   @Field()
-  @Length(3, 255)
+  @Length(3, 50)
   title: string;
 
   @Field()
-  @Length(3, 255)
+  @Length(3, 600)
   description: string;
 
+  // do we need this for update ?
   @Field({ nullable: true })
-  @IsUrl()
   imageUrl?: string;
+
+  @Field(() => GraphQLUpload, { nullable: true })
+  image?: FileUpload;
 }
--- src/resolvers/offer/OfferResolver.ts
+++ src/resolvers/offer/OfferResolver.ts
@@ -4,6 +4,12 @@ import { OfferInput } from './OfferInput';
 import { isAuth } from '../../middleware/isAuth';
 import { AppContext } from '../../types/AppContext';
 import { UserModel } from '../../entities/User';
+import os from "os";
+import path from 'path';
+import fs, { createWriteStream } from "fs";
+import { uploadImage } from '../../shared/aws/s3-medias';
+import { FileUpload } from 'graphql-upload';
+import { ManagedUpload } from 'aws-sdk/clients/s3';
 
 @Resolver()
 export class OfferResolver {
@@ -26,29 +32,151 @@ export class OfferResolver {
     @Arg("data") {
       title,
       description,
-      imageUrl,
+      image,
     }: OfferInput,
     @Ctx() ctx: AppContext,
   ): Promise<Offer> {
 
+    // Author check
     const authorId = ctx.payload?.userId;
 
     if (!authorId) {
       throw new Error("AuthorId was not found.");
     }
 
+    // User check
     const user = await UserModel.findById(authorId);
 
     if (!user) {
       throw new Error("User was not found.");
     }
 
-    return (await OfferModel.create({
+    // Image handling
+
+    const newOffer = await OfferModel.create({
       title,
       description,
-      imageUrl,
       author: user._id,
-    })).save();
+    });
+
+    const id = newOffer.id;
+    console.log('newOffer id', newOffer.id);
+
+    console.log('image', image);
+
+    if (!!image) {
+      const { filename, mimetype, createReadStream } = await image as FileUpload;
+
+      console.log('filename', filename);
+      console.log('mimetype', mimetype);
+      console.log('ok its an image');
+
+      const acceptedMimeTypes = [
+        'image/jpeg',
+        'image/png',
+      ];
+
+      if (!mimetype) {
+        console.log('No mimetype');
+        throw new Error(`L'image n'a pas un format accepté (jpeg ou png)`);
+      }
+
+      const mimeTypeIndex = acceptedMimeTypes.findIndex(mim => mim === mimetype);
+
+      if (mimeTypeIndex === -1) {
+        console.log('Invalid mimetype');
+        throw new Error(`L'image n'a pas un format accepté (jpeg ou png)`);
+      }
+
+      const folder = 'offers';
+      const subFolder = id;
+      const ext = mimeTypeIndex === 0 ? '.jpg' : '.png';
+      const fileKey = `${folder}/${subFolder}/${id}${ext}`;
+
+      // Write file locally (mandatory ?)
+
+      const localFile = path.join(os.tmpdir(), filename);
+
+      let fileSize = 0;
+      const maxSize = 1_000_000; // 1 mo
+
+      return new Promise(async (resolve, reject) => {
+        // errors from readstream are not caught by our global try catch
+        // https://github.com/adaltas/node-csv/issues/155
+        const handleError = async (error: Error) => {
+          console.log('--upload error--', error);
+
+          if (fs.existsSync(localFile)) {
+            fs.unlinkSync(localFile);
+          }
+
+          // Even if we don't make the save, there is something in base so delete it
+          await newOffer.remove();
+
+          // todo: send proper error response
+          reject(error);
+        };
+
+        try {
+
+          const readStream = createReadStream();
+
+          readStream
+            .on('error', (err) => {
+              console.log('error from createReadStream');
+              handleError(err);
+            })
+            .on('data', chunk => {
+              fileSize += (chunk as Buffer).byteLength;
+
+              if (fileSize > maxSize) {
+                const customError = new Error(`Le poids de l'image est supérieur à la limite de 5Mo.`);
+                readStream.destroy(customError);
+              }
+            })
+            .pipe(createWriteStream(localFile))
+            .on('finish', async () => {
+
+              console.log('finish');
+
+              // Upload image
+              const uploadResult = await uploadImage(localFile, fileKey);
+
+              // Delete local file
+              fs.unlinkSync(localFile);
+
+              if (!uploadResult) {
+                const customError = new Error(`L'upload de l'image a échoué.`);
+                handleError(customError);
+              }
+              const dataUpload = uploadResult as ManagedUpload.SendData;
+
+              newOffer.set({
+                imageUrl: dataUpload.Key,
+              })
+
+              const updatedNewOffer = await newOffer.save();
+
+              resolve(updatedNewOffer);
+
+            })
+            .on('error', (err) => {
+              console.log('error from createWriteStream');
+              handleError(err);
+            });
+
+        } catch (e) {
+          console.log('error trying to upload image', e);
+          handleError(e);
+        }
+      });
+
+    } else {
+      console.log('-----------new offer save----------------');
+
+      // Save in bdd and return
+      return await newOffer.save();
+    }
   }
 
 }
